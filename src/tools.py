6f937a41a8e29bc1ea8db32d19a1d20482292183
Sai Chaparala <cskrishna9306@gmail.com>
2025-11-13 00:23:56 -0800

    feat(tools): Rough implementation of the get_top_k_bq_users and get_top_k_bq_tables tool calls

--- src/tools.py
+++ src/tools.py
@@ -1,10 +1,71 @@
-from google.cloud import bigquery
 
 # Import custom packages
-from src.config import *
+from src.config import (
+	bq_client,
+	BQ_PROJECT_ID,
+	BQ_REGION,
+)
 
-# Initialize the BQ client
-bq_client = bigquery.Client(project=BQ_PROJECT_ID)
+def get_top_k_bq_users(
+	k: int,
+):
+	"""
+	Gets the top k users w/ the most query usage from the BigQuery project.
+
+	Args:
+		k: The number of users to get
+	"""
+
+	query = f"""
+	SELECT
+		user_email,
+		SUM(total_bytes_processed) / 1024 / 1024 / 1024 AS total_gb_processed,
+		AVG(total_bytes_processed) / 1024 / 1024 / 1024 AS avg_gb_processed,
+		(SUM(total_bytes_processed) / 1024 / 1024 / 1024) * 0.0048 AS total_cost
+	FROM {BQ_REGION}.INFORMATION_SCHEMA.JOBS
+	WHERE job_type = "QUERY"
+	GROUP BY 1
+	ORDER BY 2 DESC
+	LIMIT {k}
+	"""
+
+	# Start the query job
+	query_job = bq_client.query(query).result()
+	
+	# print(query_job.to_dataframe())
+	
+	return query_job.to_dataframe()
+
+def get_top_k_bq_tables(
+	k: int,
+):
+	"""
+	Gets the top k tables from the BigQuery project.
+
+	Args:
+		k: The number of tables to get
+	"""
+
+	query = f"""
+	SELECT
+		project_id,
+		table_name,
+		creation_time,
+		total_rows,
+		total_physical_bytes / 1024 / 1024 / 1024 AS size_gb,
+	FROM {BQ_REGION}.INFORMATION_SCHEMA.TABLE_STORAGE
+	WHERE deleted=FALSE
+ 	ORDER BY size_gb DESC
+	LIMIT {k}
+	"""
+
+	# Start the query job
+	query_job = bq_client.query(query).result()
+	
+	print(query_job.to_dataframe())
+	
+	return query_job.to_dataframe()
+
 
 def delete_bq_table(
 	dataset_id: str,
@@ -26,20 +87,20 @@ def delete_bq_table(
 	return
 
 def delete_bq_dataset(
-	dataset_id: str,
+    dataset_id: str,
 ) -> None:
 	"""
-	Deletes the provided BQ dataset and all the tables within it.	
+	Deletes the provided BQ dataset and all the tables within it.
 
 	Args:
 		dataset_id: The dataset to delete
 	"""
 
 	bq_client.delete_dataset(
-		dataset_id=f"{BQ_PROJECT_ID}.{dataset_id}",
-		delete_contents=True,
-		not_found_ok=True,
+	    dataset_id=f"{BQ_PROJECT_ID}.{dataset_id}",
+	    delete_contents=True,
+	    not_found_ok=True,
 	)
-	
+
 	return
 
